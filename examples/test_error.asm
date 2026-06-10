b0502fe18a67c2b10ff82e81fc7a6e76dd7ff38e
Iván García <ivangarciaa1175@gmail.com>
2026-06-10 00:37:29 -0600

    Modificación test_error

--- examples/test_error.asm
+++ examples/test_error.asm
@@ -1,65 +1,44 @@
 ; ----------------------------------------------------
-; test_error.asm : Ejemplo con errores de entrada
-; test con errores intencionados
-; para probar el manejo de errores del ensamblador/traductor
+; test_erro.asm : Version con errores para pruebas
 ; ----------------------------------------------------
 
-; --------------- Datos -----------------
-DATA1:
-    DD 12345678
-DATA2:
-    DD 100
+SECTION .text
+GLOBAL main
 
-; --------------- Programa ---------------
-INICIO:
+main:
+    MOV EAX, 10
+    MOV EBX, 4
+
     ; ERROR 1: Mnemónico desconocido (MOOV en lugar de MOV)
-    MOOV EAX, 10
+    MOOV ECX, 5
 
-    ; ERROR 2: Registro inválido (EBX5 no existe)
-    MOV EBX5, 4
-
-    ; ERROR 3: Operandos incompatibles de tamaño (EAX 32-bit vs BL 8-bit)
-    ADD EAX, BL
-
-    ; ERROR 4: Operando faltante (ADD necesita dos operandos)
-    ADD EAX
+    ADD EAX, EBX
+    SUB EBX, 1
 
     CMP EAX, 20
     JE ETQ_IGUAL
     JNE ETQ_NOIGUAL
 
 ETQ_IGUAL:
-    ; ERROR 5: Instrucción OR con demasiados operandos
-    OR EAX, EBX, ECX
-
+    OR EAX, EBX
     JMP CONT1
 
 ETQ_NOIGUAL:
     XOR EBX, EBX
 
 CONT1:
-    ; ERROR 6: Memoria a memoria no permitida (dos operandos memoria)
-    MOV [EAX], [EBX]
+    MOV [EBX], EAX
+    MOV ECX, [EBX+4]
 
-    ; ERROR 7: Modo de direccionamiento inválido (ESP no puede ser índice en SIB)
+    ; ERROR 2: Registro inválido (EBX7 no existe)
+    MOV EBX7, 0
+
     MOV ESI, 2
-    MOV EAX, [EBX+ESP*4+16]
-
-    ; ERROR 8: Valor inmediato inválido (fuera de rango para 32 bits)
-    MOV ECX, 9999999999
-
-    ; ERROR 9: Valor hexadecimal malformado (dígito G no válido en hex)
-    MOV EDX, 0xABCG
-
-    ; Instrucciones válidas intercaladas
     LEA ECX, [EBX+ESI*4+32]
     MOVZX EDX, AL
-    XCHG EAX, EBX
 
-    ; PUSH / POP / CALL / RET
     PUSH EAX
-    ; ERROR 10: PUSH sin operando
-    PUSH
+    PUSH EBX
     CALL SUBRUTINA
     POP EBX
     POP EAX
@@ -74,8 +53,31 @@ LOOP_INI:
 
 LOOP_CONT:
     INC EAX
-    ; ERROR 11: JMP a etiqueta no definida (LOOP_VACIO no existe en este archivo)
-    JMP LOOP_VACIO
+    ; ERROR 3: Etiqueta no definida
+    JMP ETIQUETA_FALSA
 
 LOOP_FIN:
     TEST EAX, EBX
+    CMP EAX, 0
+    JE FIN
+    JMP SALTO_TEST
+
+SUBRUTINA:
+    PUSH EBP
+    MOV EBP, ESP
+    MOV EAX, 66
+    POP EBP
+    RET
+
+SALTO_TEST:
+    XCHG ECX, EDX
+    JMP FIN
+
+FIN:
+    JMP FIN
+
+SECTION .data
+DATA1:
+    DD 12345678
+DATA2:
+    DD 100
