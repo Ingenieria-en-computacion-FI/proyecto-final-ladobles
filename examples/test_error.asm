73fbb9dfd2ac84158498f1813c680fd86b818698
Iván García <ivangarciaa1175@gmail.com>
2026-06-10 00:42:20 -0600

    Crear nuevo test_error

--- examples/test_error.asm
+++ examples/test_error.asm
@@ -1,5 +1,5 @@
 ; ----------------------------------------------------
-; test_erro.asm : Version con errores para pruebas
+; test_error.asm : Version con errores para pruebas
 ; ----------------------------------------------------
 
 SECTION .text
@@ -7,77 +7,7 @@ GLOBAL main
 
 main:
     MOV EAX, 10
-    MOV EBX, 4
-
-    ; ERROR 1: Mnemónico desconocido (MOOV en lugar de MOV)
-    MOOV ECX, 5
-
-    ADD EAX, EBX
-    SUB EBX, 1
-
-    CMP EAX, 20
-    JE ETQ_IGUAL
-    JNE ETQ_NOIGUAL
-
-ETQ_IGUAL:
-    OR EAX, EBX
-    JMP CONT1
-
-ETQ_NOIGUAL:
-    XOR EBX, EBX
-
-CONT1:
-    MOV [EBX], EAX
-    MOV ECX, [EBX+4]
-
-    ; ERROR 2: Registro inválido (EBX7 no existe)
-    MOV EBX7, 0
-
-    MOV ESI, 2
-    LEA ECX, [EBX+ESI*4+32]
-    MOVZX EDX, AL
-
-    PUSH EAX
-    PUSH EBX
-    CALL SUBRUTINA
-    POP EBX
-    POP EAX
-
-    MOV ECX, 5
-
-LOOP_INI:
-    DEC ECX
-    CMP ECX, 0
-    JG LOOP_CONT
-    JMP LOOP_FIN
-
-LOOP_CONT:
-    INC EAX
-    ; ERROR 3: Etiqueta no definida
-    JMP ETIQUETA_FALSA
-
-LOOP_FIN:
-    TEST EAX, EBX
-    CMP EAX, 0
-    JE FIN
-    JMP SALTO_TEST
-
-SUBRUTINA:
-    PUSH EBP
-    MOV EBP, ESP
-    MOV EAX, 66
-    POP EBP
+    JMP etiqueta_que_no_existe    ; ERROR: etiqueta no definida
+    MOVXX EBX, 20                 ; ERROR: instrucción inválida
+    MOV EAX, @5                   ; ERROR: carácter @ no válido
     RET
-
-SALTO_TEST:
-    XCHG ECX, EDX
-    JMP FIN
-
-FIN:
-    JMP FIN
-
-SECTION .data
-DATA1:
-    DD 12345678
-DATA2:
-    DD 100
